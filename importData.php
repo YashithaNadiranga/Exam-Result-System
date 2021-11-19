a3d274eee7fecd98f94ffadecbb1966e909abc2c
Yashitha Nadiranga <yasitha.online100@gmail.com>
2021-11-19 10:17:25 +0530

    Init

---
+++ importData.php
@@ -0,0 +1,51 @@
+<?php
+// Load the database configuration file
+include_once 'dbConfig.php';
+
+if(isset($_POST['importSubmit'])){
+    
+    // Allowed mime types
+    $csvMimes = array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'text/plain');
+    
+    // Validate whether selected file is a CSV file
+    if(!empty($_FILES['file']['name']) && in_array($_FILES['file']['type'], $csvMimes)){
+        
+        // If the file is uploaded
+        if(is_uploaded_file($_FILES['file']['tmp_name'])){
+            
+            // Open uploaded CSV file with read-only mode
+            $csvFile = fopen($_FILES['file']['tmp_name'], 'r');
+            
+            // Skip the first line
+            fgetcsv($csvFile);
+            
+            // Parse data from CSV file line by line
+            while(($line = fgetcsv($csvFile)) !== FALSE){
+                // Get row data
+                $name   = $line[0];
+                $date  = $line[1];
+                $result  = $line[2];
+                $status = $line[3];
+                $grade = $line[4];
+                $sname = $line[5];
+
+                
+                // Insert member data in the database
+                $db->query("INSERT INTO exam (name, date, result, status, grade, sname) VALUES ('".$name."', '".$date."', '".$result."', '".$status."', '".$grade."', '".$sname."')");
+                
+            }
+            
+            // Close opened CSV file
+            fclose($csvFile);
+            
+            $qstring = '?status=succ';
+        }else{
+            $qstring = '?status=err';
+        }
+    }else{
+        $qstring = '?status=invalid_file';
+    }
+}
+
+// Redirect to the listing page
+header("Location: index.php".$qstring);
